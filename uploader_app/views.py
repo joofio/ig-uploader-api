129cff586720ccfa1450bfae7c904b2bcdab2001
João Almeida <joaofilipe90@gmail.com>
2024-04-04 20:13:14 +0100

    starting up

--- uploader_app/views.py
+++ uploader_app/views.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from uploader_app import app
 import subprocess
+import base64
 
 print(app.config)
 
@@ -16,43 +17,77 @@ def hello():
 @app.route("/upload-ig", methods=["POST"])
 def upig():
     data = request.json
+    print(data)
     serverBase = data["serverBase"]
-    packageId = data["packageId"]
-    usePUT = data["usePUT"]
-    loadRecursively = data["loadRecursively"]
-    packagebase64 = data["packagebase64"]
+    packageId = data.get("packageId", None)
+    usePUT = data.get("usePUT", True)
+    loadRecursively = data.get("loadRecursively", False)
+    packagebase64 = data.get("packagebase64", None)
     if packageId and packagebase64:
         return 404, "Package ID OR package base64 must be provided. Not both."
     if not serverBase:
         return 404, "Server base must be provided."
     if packagebase64:
         # save to disk
-        with open("/tmp/package.zip", "wb") as f:
-            f.write(packagebase64)
+        package_bytes = base64.b64decode(packagebase64)
+        with open("package.zip", "wb") as f:
+            f.write(package_bytes)
 
-        with open("/tmp/output.log", "w") as output:
-            subprocess.call(
-                "docker run --rm jkiddo/ember ",
-                shell=True,
-                stdout=output,
-                stderr=output,
-            )
-            # docker run -v /Users/jkv/.fhir:/home/nonroot/.fhir jkiddo/ember --location=https://build.fhir.org/ig/hl7-eu/gravitate-health/package.tgz
-
-        return 200, "Success"
-    if packageId:
-        with open("/tmp/output.log", "w") as output:
-            subprocess.call(
-                "docker run --rm jkiddo/ember --packageId="
-                + packageId
+        with open("logs/output.log", "w") as output:
+            command = (
+                "docker run --rm jkiddo/ember --location=package.zip"
                 + " --serverBase="
-                + serverBase
+                + str(serverBase)
                 + " --usePUT="
-                + usePUT
+                + str(usePUT)
                 + " --loadRecursively="
-                + loadRecursively,
+                + str(loadRecursively)
+            )
+            print(command)
+            subprocess.call(
+                command,
                 shell=True,
                 stdout=output,
                 stderr=output,
             )
-        return 200, "Success"
+
+        with open("logs/output.log", "r") as output:
+            if "ERROR" in output.read():
+                with open("logs/output.log", "r") as error_file:
+                    error_content = error_file.read()
+                return jsonify({"result": "Success", "message": error_content}), 500
+
+        return jsonify(
+            {"result": "Success", "message": "Package has been uploaded successfully"}
+        )
+
+    return 200, "Success"
+    if packageId:
+        with open("logs/output.log", "w") as output:
+            command = (
+                "docker run --rm jkiddo/ember --packageId="
+                + str(packageId)
+                + " --serverBase="
+                + str(serverBase)
+                + " --usePUT="
+                + str(usePUT)
+                + " --loadRecursively="
+                + str(loadRecursively)
+            )
+            print(command)
+            subprocess.call(
+                command,
+                shell=True,
+                stdout=output,
+                stderr=output,
+            )
+
+        with open("logs/output.log", "r") as output:
+            if "ERROR" in output.read():
+                with open("logs/output.log", "r") as error_file:
+                    error_content = error_file.read()
+                return jsonify({"result": "Success", "message": error_content}), 500
+
+        return jsonify(
+            {"result": "Success", "message": "Package has been uploaded successfully"}
+        )
